2c28460b683434bf7f38500ccb7956a07910ef72
Abhii0911 <abhii0911@gmail.com>
2021-07-05 12:21:28 +0530

    Added homepage files and scss styles

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
- 
+import './homepage.styles.scss'
 const HomePage= () => {
     return (
     <div className="homepage"> 
@@ -7,6 +7,30 @@ const HomePage= () => {
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">DENIM</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">GILRS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">BOYS</h1>
                         <span className="subtitle">SHOP NOW</span>
                 </div>
             </div>
